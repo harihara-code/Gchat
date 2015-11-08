697eceea103148a57dad77e4ec006ad9fd70a7d4
harihara95 <harihara95@gmail.com>
2015-11-08 19:32:39 +0530

    NetworkInterface.java is added

---
+++ NetworkInterface.java
@@ -0,0 +1,9 @@
+package Gchat;
+
+import java.net.*;
+public interface NetworkInterface {
+   Socket createSocket(String server,int port);
+   ServerSocket createServerSocket(int port);
+   boolean sendMessage(Socket socket,String message);
+   String receiveMessage(Socket socket);
+}
