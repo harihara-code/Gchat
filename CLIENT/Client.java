cbcdd88aece369e40de2556f00eb611926afe5c5
harihara95 <harihara95@gmail.com>
2015-11-08 22:00:43 +0530

    CLIENT/Client.java is updated

--- CLIENT/Client.java
+++ CLIENT/Client.java
@@ -7,7 +7,9 @@ public class Client implements Runnable{
 
     private Socket serverSocket = null;
     private Gchat.NetworkSocket socket = null;
-    public static void main(String args[]) {
+    private volatile boolean consoleLock = false;
+
+    public static void main(String args[]) throws Exception {
     //Instantiates Gchat.Client class object
       Client clientObj = new Client();
 
@@ -24,12 +26,66 @@ public class Client implements Runnable{
     //Create new Thread to handle receiving message operation
       Thread newThread = new Thread(clientObj);
       newThread.start();
+       
+      String clientOption = null;
+      BufferedReader consoleScreen = new BufferedReader(new InputStreamReader(System.in));
+
+    do {
+    //Display Client Menu Interface
+      System.out.println("Chat - Enter c");
+      System.out.println("Exit - Enter e");
+      clientOption = consoleScreen.readLine();
+     
+    //Enable the console lock to tell client ConsoleScreen is busy in chatting.
+      clientObj.consoleLock = true;
+      if(clientOption.equals("c")){
+      	System.out.print("Enter the Message : ");
+      	String message = consoleScreen.readLine();
+
+      //Sending message to the server
+      	clientObj.socket.sendMessage(clientObj.serverSocket,message);
+      	
+      //Chat Operation is completed clientConsoleScreen is free now 
+      	clientObj.consoleLock = false;
+      }
+      else if(clientOption.equals("e")) {
+      //Sending message to the server
+      	clientObj.socket.sendMessage(clientObj.serverSocket,"quit");
+		System.out.println("Outcoming Closed");
+      //Chat Operation is completed clientConsoleScreen is free now 
+      	clientObj.consoleLock = false;
       
+	  //Wait until newThread terminate its execution
+		newThread.join();
+      	System.out.println("Incoming Closed");
+	  }
+      else {
+      //Chat Operation is completed clientConsoleScreen is free now 
+      	clientObj.consoleLock = false;
+      	System.out.println("Invalid Menu Option please try again.");
+      
+      }
 
-
+    } while(!clientOption.equals("e"));
 	}
 
 	public void run() {
-	 	
+	  String message = null;
+
+	do {
+	//Get the message from the server.
+	  message = socket.receiveMessage(serverSocket);
+	//If client Console Screen is busy. 
+	  if(consoleLock) {
+	  //Wait for client typing is completed.
+	  	while(consoleLock) {
+
+	  	}
+	  }
+	//Client Console screen is free now.
+	  System.out.println("Server :"+message);
+	 }while(!message.equals("quit"));
+
+
 	}
 }
